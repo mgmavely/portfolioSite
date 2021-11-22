85f248aef37ba60fffb34c1439cef790ed26d07b
mgmavely <mgmavely@gmail.com>
2021-11-22 17:46:01 -0500

    Updated resume link and project infos

--- src/mock/data.js
+++ src/mock/data.js
@@ -4,7 +4,7 @@ import { nanoid } from 'nanoid';
 export const headData = {
   title: 'Michael | Developer', // e.g: 'Name | Developer'
   lang: 'en', // e.g: en, es, fr, jp
-  description: 'Welcome to my website', // e.g: Welcome to my website
+  description: 'Welcome to my portfolio site!', // e.g: Welcome to my website
 };
 
 // HERO DATA
@@ -23,10 +23,10 @@ export const aboutData = {
   placement.  The placement will provide a platform to showcase my aptitude and insight for developing',
   paragraphTwo: ' C++, Python, JavaScript, HTML/CSS, Bash and Git are some of the languages I am versed in for \
   application within the software development field.  In addition, I am learning new languages from my \
-  experiences from advanced hackathons such as: YHacks, DelaHacks, and NewHacks.',
+  experiences from advanced hackathons such as: YHacks, DeltaHacks, and NewHacks.',
   paragraphThree: 'I believe my skills and experiences can be of value added to your organization or company.  Please feel \
   free to browse my resume provided below where my contact details are also available.',
-  resume: 'https://drive.google.com/file/d/1D2Y_n5XkxS69S4GZEnLz3X3AbwMl8f0l/view?usp=sharing', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/1h_SuqQf-JXOALpVMfilCXKGsZdr-7AM0/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -35,9 +35,9 @@ export const projectsData = [
     id: nanoid(),
     img: 'project2.jpg',
     title: 'Personal Discord Bot',
-    info: 'Developed a Discord Bot for proof of concept utilization with a test server.  Created as a node.js project,\
-     the Personal Discord Bot is primarily coded in JavaScript, and utilizes the DiscordAPI for access to Discord Bot controls.\
-     Current functionality features some reaction-based actions as well as an asychronous TicTacToe game that can be played with 2 \
+    info: 'Developed a Discord Bot for proof-of-concept utilization with a test server.  Created as a node.js project,\
+     the Personal Discord Bot is primarily coded in JavaScript, and utilizes the Discord API for access to Discord Bot controls.\
+     Current functionality features some reaction-based actions as well as an asynchronous Tic-tac-toe game that can be played with 2 \
      users in the server.',
     info2: '',
     url: 'https://github.com/mgmavely/Personal-Discord-Bot',
@@ -46,10 +46,10 @@ export const projectsData = [
     id: nanoid(),
     img: 'project1.jpg',
     title: 'Apple Music Playlist to Spotify',
-    info: 'A simple text-based user interface that allows the transfer of playlists from. \
+    info: 'A simple text-based user interface that allows the transfer of playlists from \
      Apple Music to Spotify.  When prompted, user can enter the link to an Apple Music playlist and soon after,\
-     A playlist on spotify will be created with all available songs from both platforms with all playlist metadata transferred. \
-     Integrated via python script and requests library in conjuction with Spotify for Developers. ',
+     a playlist on Spotify will be created with all available songs from both platforms with all playlist metadata transferred. \
+     Integrated via Python script and requests library in conjunction with Spotify for Developers. ',
     info2: '',
     url: 'https://github.com/mgmavely/apple-music-to-spotify',
   },
@@ -57,7 +57,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'project3.jpg',
     title: 'Top 10 Movies',
-    info: 'Built a Flask based Web Application with the following libraries: WTForms, SQLite, and SQLAlchemy.  It allows users to populate site with their 10 favourite movies via search, and uses the IMDB API to gather metadata for UI.  Users can add/delete/change entries into their list of favourite movies with a few clicks.',
+    info: 'Built a Flask based Web Application with the following libraries: WTForms, SQLite, and SQLAlchemy.  It allows users to populate site with their 10 favorite movies via search, and uses the IMDB API to gather metadata for UI.  Users can add/delete/change entries into their list of favourite movies with a few clicks.',
     info2: '',
     url: 'https://github.com/mgmavely/top-10-movies',
   },
